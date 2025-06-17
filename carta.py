06282e36d4fe1157873b426c1468544277fd3163
wettooo777 <welliton.souza.oliveira@escola.pr.gov.br>
2025-06-17 12:19:38 +0000

    carta.py terminado

---
+++ carta.py
@@ -0,0 +1,7 @@
+email_destinatario = input("Digite o email do destinatário: ")
+mensagem = input("Digite a mensagem: ")
+email_remetente = input("Digite o seu email: ")
+data = input("Digite o dia de hoje: ")
+
+print("\n---Seu Email---")
+print(f"Olá {email_destinatario}, \n {mensagem} \n\n Atenciosamente, \n {email_remetente} \n\n {data}")
